36846b99037b322fa4a29155c2ddb028147fd80d
jorgezamy <jorge.zamora60@hotmail.com>
2024-10-02 17:58:06 -0600

    feat: services registered

--- Kynaxis/Program.cs
+++ Kynaxis/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<HackerNewsService>();
+builder.Services.AddMemoryCache();
+
+builder.Services.AddScoped<HackerNewsService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
